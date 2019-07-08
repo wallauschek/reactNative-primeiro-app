d9004e84be6b382aee7834bb0a518b6ae490b51d
Pedro Wallauschek <pedro@pedrowallauschek.com.br>
2019-07-07 22:05:48 -0300

    exibindo repositorio com webview

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -26,6 +26,7 @@ export default class User extends Component {
   static propTypes = {
     navigation: PropTypes.shape({
       getParam: PropTypes.func,
+      navigate: PropTypes.func,
     }).isRequired,
   };
 
@@ -68,6 +69,12 @@ export default class User extends Component {
     });
   };
 
+  handleNavigate = repository => {
+    const { navigation } = this.props;
+
+    navigation.navigate('Repository', { repository });
+  };
+
   render() {
     const { navigation } = this.props;
     const { stars, loading, refreshing } = this.state;
@@ -94,7 +101,7 @@ export default class User extends Component {
               data={stars}
               keyExtractor={star => String(star.id)}
               renderItem={({ item }) => (
-                <Starred>
+                <Starred onPress={() => this.handleNavigate(item)}>
                   <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
                   <Info>
                     <Title>{item.name}</Title>
